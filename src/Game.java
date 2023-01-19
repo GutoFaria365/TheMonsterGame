a683d9c126b88795a72c47032b2c7ca18a859c28
GutoFaria365 <augusto.filipefaria@gmail.com>
2023-01-19 15:17:50 +0000

    Game start

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,11 @@
 public class Game {
-    public void GameStart (Player p1, Player p2) {
+    public void gameStart (Player p1, Player p2) {
         if (p1.getNumberOfMonsters() == p2.getNumberOfMonsters()) {
             System.out.println("Welcome " + p1.getPlayerName() + " and " + p2.getPlayerName());
             System.out.println("The game will start with " + p1.getNumberOfMonsters() + " monsters!!!");
 
         } else {
-            System.out.println("The number of monsters selected doesn't match, select the same value for bot players!!");
+            System.out.println("The number of monsters selected doesn't match, select the same value for both players!!");
 
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello People!");
         System.out.println("Boas");
+
+        Player p1 = new Player("Boda", 5);
+        Player p2 = new Player("Calimero", 5);
+
+        Game game1 = new Game();
+
+        game1.gameStart(p1,p2);
+
+
     }
 
 }
