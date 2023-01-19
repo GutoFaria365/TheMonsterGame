ec303f558e1feac1da1b92e2a6a1d10483044b46
tiagosilva28 <tiagosilva28@live.com.pt>
2023-01-19 14:57:30 +0000

    Create Players

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,15 @@
 public class Player {
+    private String name;
+    private int numberOfMonsters;
+    private Monster[] monsters = new Monster[numberOfMonsters];
+
+
+
+    public Player(String name, int numberOfMonsters) {
+        this.name = name;
+        this.numberOfMonsters = numberOfMonsters;
+        this.monsters = new Monster[numberOfMonsters];
+    }
+
 
 }
