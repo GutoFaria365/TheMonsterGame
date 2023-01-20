76fe501bd256278f086fe3982980a03b1ed0a932
GutoFaria365 <augusto.filipefaria@gmail.com>
2023-01-20 14:42:59 +0000

    Game start

--- src/Monster.java
+++ src/Monster.java
@@ -33,6 +33,7 @@ public abstract class Monster {
         if(this.health <= 0) {
             this.isDead = true;
             System.out.println(this.getName() + " has been slain!!!");
+            return true;
         } else
             System.out.println(this.getName() + " has " + this.health + " health remaining.");
         return false;
