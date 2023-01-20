c376446a4a90ca28757bf223ca6f7d2e654d700b
GutoFaria365 <augusto.filipefaria@gmail.com>
2023-01-20 10:13:33 +0000

    Game start

--- src/Mummy.java
+++ src/Mummy.java
@@ -2,13 +2,12 @@ public class Mummy extends Monster {
 
     public Mummy(String name, int attackPower, int health) {
         super(name, attackPower, health);
-        this.specialPower = 2; //if it attacks a third time in a row it will lose some health
+        //if it attacks a third time in a row it will lose some health
     }
     public Mummy() {
         this.name = "Mummy";
         this.attackPower = 30;
         this.health = 80;
-        this.specialPower = 2;
 
     }
 
--- src/Werewolf.java
+++ src/Werewolf.java
@@ -1,13 +1,12 @@
 public class Werewolf extends Monster {
     public Werewolf(String name, int attackPower, int health) {
         super(name, attackPower, health);
-        this.specialPower = 0; //no special
+        //no special
     }
     public Werewolf() {
         this.name = "Werewolf";
         this.attackPower = 20;
         this.health = 120;
-        this.specialPower = 0;
     }
 
     @Override
