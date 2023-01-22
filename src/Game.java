045778a7c9f69c366e99d2b74e96317b5b1aedbd
GutoFaria365 <augusto.filipefaria@gmail.com>
2023-01-22 23:51:58 +0000

    Game start

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,15 @@
 public class Game {
     private Player playerOne;
     private Player playerTwo;
-    private int roundCount;
+    public static int roundCount;
 
     public int getRoundCount() {
         return roundCount;
     }
 
     public Game() {
-        this.playerOne = new Player("Tiago", 5);
-        this.playerTwo = new Player("Beatriz", 5);
+        this.playerOne = new Player("Tiago", 1);
+        this.playerTwo = new Player("Beatriz", 1);
         this.roundCount = 1;
     }
 
@@ -29,8 +29,8 @@ public class Game {
     public void randomMonster(Player player) {
         for (int i = 0; i < player.getNumberOfMonsters(); i++) {
             switch (Utilities.generateGuessingNumber()) {
-                case 1 -> player.monsters[i] = new Werewolf();
-                case 2 -> player.monsters[i] = new Vampire();
+                case 1 -> player.monsters[i] = new Mummy();
+                case 2 -> player.monsters[i] = new Mummy();
                 case 3 -> player.monsters[i] = new Mummy();
             }
         }
--- src/Mummy.java
+++ src/Mummy.java
@@ -1,7 +1,7 @@
 public class Mummy extends Monster {
-
-
-
+   public static int countAttacks = 0;
+   public static int consecutiveAttack = 0;
+   public static int consecutiveAttack2 = 0;
     public Mummy(String name, int attackPower, int health) {
         super(name, attackPower, health);
 
@@ -15,26 +15,43 @@ public class Mummy extends Monster {
     }
     @Override
     public void monsterAttack(Monster monster) {
-        int consecutiveAttack = 0;
-        int countAttacks = 0;
-        Game round = new Game();
-
-        if (consecutiveAttack == 0) {
-            consecutiveAttack = round.getRoundCount();
-            countAttacks++;
-        } else if (round.getRoundCount() - consecutiveAttack == 2) {
-            countAttacks++;
+        if (this.consecutiveAttack == 0) {
+            this.consecutiveAttack = Game.roundCount;
+            this.countAttacks += 1;
+            System.out.println(this.countAttacks + "round1a" + this.consecutiveAttack + Game.roundCount);
+        } else if (Game.roundCount - consecutiveAttack == 1) {
+            this.consecutiveAttack = Game.roundCount;
+            this.countAttacks += 1;
+            System.out.println(this.countAttacks + "round2a" + this.consecutiveAttack + Game.roundCount);
+        } else if (Game.roundCount - consecutiveAttack > 1) {
+            this.consecutiveAttack = 0;
+            this.countAttacks = 0;
+            System.out.println(this.countAttacks + "round3a" + this.consecutiveAttack + Game.roundCount);
         } else {
-            consecutiveAttack = 0;
-            countAttacks = 0;
+            System.out.println(this.countAttacks + "round4a" + this.consecutiveAttack2 + Game.roundCount);
+            if (this.consecutiveAttack2 == 0) {
+                this.consecutiveAttack2 = Game.roundCount;
+                this.countAttacks += 1;
+                System.out.println(this.countAttacks + "round1a" + this.consecutiveAttack2 + Game.roundCount);
+            } else if (Game.roundCount - consecutiveAttack2 == 1) {
+                this.consecutiveAttack2 = Game.roundCount;
+                this.countAttacks += 1;
+                System.out.println(this.countAttacks + "round2a" + this.consecutiveAttack2 + Game.roundCount);
+            } else if (Game.roundCount - consecutiveAttack2 > 1) {
+                this.consecutiveAttack2 = 0;
+                this.countAttacks = 0;
+                System.out.println(this.countAttacks + "round3a" + this.consecutiveAttack2 + Game.roundCount);
+            } else;
         }
-
-        if (countAttacks < 3) {
+        if (this.countAttacks < 4) {
             monster.health -= this.getAttackPower();
-            System.out.println(this.getName() + " is attacking " + monster.getName() + " for " + this.getAttackPower() + "  points of damage!");
+            System.out.println(this.getName() + this.hashCode() + " is attacking " + monster.getName() + " for " + this.getAttackPower() + "  points of damage!");
             monster.isDead();
         } else {
-            System.out.println("GET REKT");
+            System.out.println(this.getName() + this.hashCode()  + " attacked itself in it's own confusion");
+            System.out.println(this.getName() + this.hashCode()  + " has " + this.health + " health remaining.");
+            this.health -= this.getAttackPower();
+            this.countAttacks = 0;
             //if it attacks a third time in a row it will lose some health
         }
     }
